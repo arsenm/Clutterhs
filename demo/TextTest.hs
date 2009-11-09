8148cce6b75bb44b1ee3afb0dc158a25d4e64862
Matt Arsenault <arsenm2@rpi.edu>
2009-11-09 14:53:50 -0500

    Minor addition to TextTest

--- demo/TextTest.hs
+++ demo/TextTest.hs
@@ -1,6 +1,7 @@
 
 import Prelude
 import qualified Prelude as P
+import Data.Maybe
 
 import Graphics.UI.Clutter
 import System.Glib.Signals
@@ -14,7 +15,15 @@ main = do
   text <- textNewFull "Webdings 12" "Comic Sans is the best font" (Color 255 0 0 255)
   set text [ actorPosition := (100, 200) ]
 
+  let black = fromJust (colorFromString "black")
+  pwText <- textNewFull "Sans 10" "This is a secret password" black
+--  set pwText [ textPasswordChar := '*' ]
+  --FIXME: Take  a charactor
+  set pwText [ textPasswordChar := 12 ]
+  set pwText [ actorPosition := (50, 300) ]
+
   containerAddActor stage text
+  containerAddActor stage pwText
 
   actorShowAll stage
 

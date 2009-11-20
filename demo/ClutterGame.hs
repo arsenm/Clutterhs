9a8486e54bca9bf1633f5fac47bb7e894f8a339a
Matt Arsenault <arsenm2@rpi.edu>
2009-11-20 11:16:07 -0500

    THE CLUTTER GAME WORKS AGAIN!!!!

--- demo/ClutterGame.hs
+++ demo/ClutterGame.hs
@@ -33,7 +33,7 @@ createRectangles stage size (_, recs, i) col = do
   containerAddActor stage rec
   set rec [ actorHeight := size,
             actorWidth := size,
-            actorAnchorPointGravity := GravityCenter,
+            actorAnchorGravity := GravityCenter,
             actorY := i*size + size/2 ]
 
   width <- get stage actorWidth
@@ -41,19 +41,18 @@ createRectangles stage size (_, recs, i) col = do
 
   return (Just anim, rec:recs, i+1)
 
-scatterRectangle :: Float -> Float -> Rectangle -> IO ()
+scatterRectangle :: Float -> Float -> Rectangle -> IO Animation
 scatterRectangle h w rec = do
   rnd1 <- randomIO
   rnd2 <- randomIO
   --rotation_angle change makes it stop spinning. don't know why it's still
   --spinning here, it really should have stopped when the timeline did.
   (ang,_,_,_) <- actorGetRotation rec ZAxis
-  animate rec EaseOutBounce 3000 ("x", rnd1 * w)
-                                 ("y", rnd2 * h / 2 + h / 2)
-                                 ("rotation-angle-z", ang)
-                                 ("opacity", 0::Word8)
+  animate rec EaseOutBounce 3000 [ actorX :-> rnd1 * w,
+                                   actorY :-> rnd2 * h / 2 + h / 2,
+                                   actorRotationAngleZ :-> ang,
+                                   actorOpacity :-> 0 ]
 
---FIXME: Why did this need a type signature?
 completedAnimation :: Stage -> IO Animation
 completedAnimation stage = do
   text <- textNewWithText "Bitstream Vera Sans 40" "Congratulations!"
@@ -61,12 +60,12 @@ completedAnimation stage = do
   h <- get text actorHeight
   stageheight <- get stage actorHeight
   set text [textColor := white,
-            actorAnchorPointGravity := GravityCenter,
+            actorAnchorGravity := GravityCenter,
             actorX := w / 2,
             actorY := (-h)  -- off stage
            ]
   containerAddActor stage text
-  animate text EaseOutBounce 3000 ("y", stageheight / 2)
+  animate text EaseOutBounce 3000 [actorY :-> stageheight / 2]
 
 main = do
   clutterInit
@@ -80,8 +79,6 @@ main = do
   width <- get stage actorWidth
   let size = height / fromIntegral (length colors)
 
-  --TODO: Can do animations with signals in name
-  --FIXME: This takes...2 arguments....function in your gvalue...triple...this...requires making work.
   (Just finAnim, recs, _) <- foldM (createRectangles stage size) (Nothing, [], 0) colors
   tml <- fmap fromJust $ get finAnim animationTimeline
 

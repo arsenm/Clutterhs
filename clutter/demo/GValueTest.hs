9146c1e6df999f6463b4440dac67906e167ef404
Matt Arsenault <arsenm2@rpi.edu>
2009-11-23 12:55:53 -0500

    Add simple test to play with gvalue things

---
+++ clutter/demo/GValueTest.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import Prelude
+import qualified Prelude as P
+
+import Graphics.UI.Clutter
+
+import qualified Graphics.UI.Clutter.GTypes as CGT
+import qualified System.Glib.GTypeConstants as GT
+import System.Glib.GObject
+
+
+
+main = do
+  clutterInit
+
+  tml <- timelineNew 3000
+  alp <- alphaNewFull tml Linear
+  bo <- behaviourOpacityNew (Just alp) 255 10
+
+  let a = isA bo CGT.behaviour
+  let b = isA bo CGT.behaviouropacity
+  let c = isA bo CGT.behaviourellipse
+  let d = isA bo GT.object
+  print [a,b,c,d]
+
+  clutterMain
+

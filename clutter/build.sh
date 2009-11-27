404180f4bf28d548b9b950a12062a64da2df3cf6
Matt Arsenault <arsenm2@rpi.edu>
2009-11-27 15:05:44 -0500

    Add minor convenience to build.sh

--- clutter/build.sh
+++ clutter/build.sh
@@ -4,4 +4,12 @@ runghc Setup.hs configure --user
 runghc Setup.hs build
 runghc Setup.hs install
 
+if [ $1 ]; then
+    echo "Building demos"
+    for i in `ls demo/*.hs`
+      do
+        ghc --make -hide-package transformers $i
+    done
+fi
+
 

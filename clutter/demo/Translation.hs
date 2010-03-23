d1c4166e5c6c82be4d30f0c6301d6de42e4553fe
Matt Arsenault <arsenm2@rpi.edu>
2010-03-22 21:55:51 -0400

    Add Translation.hs

---
+++ clutter/demo/Translation.hs
@@ -0,0 +1,22 @@
+
+import System.Glib.Attributes
+import Graphics.UI.Clutter hiding (show)
+import Random
+
+main = do
+  clutterInit
+  stg <- stageGetDefault
+  txt <- textNewWithText "sans 30" "Translation"
+
+  set txt [ actorWidth := 100,
+            actorHeight := 30,
+            actorX := 10,
+            actorY := 200
+          ]
+
+  stg `containerAddActor` txt
+  animate txt Linear 2000 [ actorX :-> 400 ]
+
+  actorShowAll stg
+  clutterMain
+
